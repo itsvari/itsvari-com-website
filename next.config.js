df11796dd048c4afcf2d67c6b01cc50f6439ea86
itsvari <13640661+itsvari@users.noreply.github.com>
2025-03-04 17:35:21 +0800

    heroui init

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  images: {
+    unoptimized: true,
+    domains: [],
+  },
+};
+
+module.exports = nextConfig;
